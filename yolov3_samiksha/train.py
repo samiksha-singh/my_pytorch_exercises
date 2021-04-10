fbdfb62998d6454556582c31473c6a8f8b2fca5d
samiksha <samiksha@gmail.com>
2021-04-11 03:58:22 +0530

    rearranged argsparse statements

--- yolov3_samiksha/train.py
+++ yolov3_samiksha/train.py
@@ -113,17 +113,18 @@ def main(opt):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--pretrained_weights", type=str, help="if specified starts from checkpoint model")
+    parser.add_argument("--pretrained_weights", type=str,
+                        help="Whether to load any weights. Can be original darknet weights or prev pytorch saved checkpoints")
+    parser.add_argument("--root_train", type=Path, default="/home/samiksha/dataset/voc2007/train",
+                        help="root directory for train")
+    parser.add_argument("--root_test", type=Path, default="/home/samiksha/dataset/voc2007/test",
+                        help="root directory for test")
 
     parser.add_argument("--epochs", type=int, default=300, help="number of epochs")
     parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
-    parser.add_argument("--checkpoint_interval", type=int, default=1, help="interval between saving model weights")
+    parser.add_argument("--checkpoint_interval", type=int, default=20, help="interval between saving model weights")
     parser.add_argument("--evaluation_interval", type=int, default=1, help="interval evaluations on validation set")
-    parser.add_argument("--root_train", type=Path, default="/home/samiksha/dataset/voc2007/train",
-                        help="root directory for train")
-    parser.add_argument("--root_test", type=Path, default="/home/samiksha/dataset/voc2007/test",
-                        help="root directory for test")
     opt = parser.parse_args()
     main(opt)
